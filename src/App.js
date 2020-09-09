831ae7a415d00a0b4ae10c86a990c3976c636c62
Maximilian Coghlan <max.coghlan@icloud.com>
2020-09-09 17:38:32 +0100

    refactored filter into component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { nanoid } from "nanoid";
 // Components
 import Form from "./components/Form";
 import ToDoList from "./components/ToDoList";
+import Filter from "./components/Filter";
 
 function App() {
   // State
@@ -47,37 +48,6 @@ function App() {
   };
 
   // Filter
-  const handleFilter = (e) => {
-    setFilter(e.target.value);
-    changeClass(e.target.value);
-  };
-
-  const changeClass = (filter) => {
-    switch (filter) {
-      case "active":
-        setFilterBtnClass({ all: "", active: "__active", completed: "" });
-        break;
-      case "completed":
-        setFilterBtnClass({ all: "", active: "", completed: "__active" });
-        break;
-      default:
-        setFilterBtnClass({ all: "__active", active: "", completed: "" });
-        break;
-    }
-  };
-
-  useEffect(() => {
-    switch (filter) {
-      case "completed":
-        setFilteredList(toDoList.filter((todo) => todo.completed === true));
-        break;
-      case "active":
-        setFilteredList(toDoList.filter((todo) => todo.completed === false));
-        break;
-      default:
-        setFilteredList(toDoList);
-    }
-  }, [filter, toDoList]);
 
   return (
     <div className="App">
@@ -88,37 +58,16 @@ function App() {
         toDoList={toDoList}
         setToDoList={setToDoList}
       />
-      <div className="filter">
-        <ul>
-          <li>
-            <button
-              value="all"
-              className={`filter-btn ${filterBtnClass.all}`}
-              onClick={handleFilter}
-            >
-              All
-            </button>
-          </li>
-          <li>
-            <button
-              value="active"
-              className={`filter-btn ${filterBtnClass.active}`}
-              onClick={handleFilter}
-            >
-              Active
-            </button>
-          </li>
-          <li>
-            <button
-              value="completed"
-              className={`filter-btn ${filterBtnClass.completed}`}
-              onClick={handleFilter}
-            >
-              Completed
-            </button>
-          </li>
-        </ul>
-      </div>
+      <Filter
+        filter={filter}
+        setFilter={setFilter}
+        filterBtnClass={filterBtnClass}
+        setFilterBtnClass={setFilterBtnClass}
+        filteredList={filteredList}
+        setFilteredList={setFilteredList}
+        toDoList={toDoList}
+      />
+
       <ToDoList
         toDoList={toDoList}
         setToDoList={setToDoList}
---
+++ src/components/Filter.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+
+const Filter = (props) => {
+  const handleFilter = (e) => {
+    props.setFilter(e.target.value);
+    changeClass(e.target.value);
+  };
+
+  const changeClass = (filter) => {
+    switch (filter) {
+      case "active":
+        props.setFilterBtnClass({ all: "", active: "__active", completed: "" });
+        break;
+      case "completed":
+        props.setFilterBtnClass({ all: "", active: "", completed: "__active" });
+        break;
+      default:
+        props.setFilterBtnClass({ all: "__active", active: "", completed: "" });
+        break;
+    }
+  };
+
+  useEffect(() => {
+    switch (props.filter) {
+      case "completed":
+        props.setFilteredList(
+          props.toDoList.filter((todo) => todo.completed === true)
+        );
+        break;
+      case "active":
+        props.setFilteredList(
+          props.toDoList.filter((todo) => todo.completed === false)
+        );
+        break;
+      default:
+        props.setFilteredList(props.toDoList);
+    }
+  }, [props.filter, props.toDoList]);
+  return (
+    <div className="filter">
+      <ul>
+        <li>
+          <button
+            value="all"
+            className={`filter-btn ${props.filterBtnClass.all}`}
+            onClick={handleFilter}
+          >
+            All
+          </button>
+        </li>
+        <li>
+          <button
+            value="active"
+            className={`filter-btn ${props.filterBtnClass.active}`}
+            onClick={handleFilter}
+          >
+            Active
+          </button>
+        </li>
+        <li>
+          <button
+            value="completed"
+            className={`filter-btn ${props.filterBtnClass.completed}`}
+            onClick={handleFilter}
+          >
+            Completed
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Filter;
